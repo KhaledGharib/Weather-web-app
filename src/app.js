76fab32b9715419c55b5cfa75021cf987e868525
Khaled Gharib <frkhwg@gmail.com>
2023-04-08 11:42:12 +0300

    delete nav section

--- src/app.js
+++ src/app.js
@@ -1,94 +1,75 @@
-const express =require('express')
-const path = require('path')
-const hbs = require('hbs')
-const app = express()
-const geocode = require('./utils/geocode')
-const forecast = require('./utils/forecast')
-const port = process.env.PORT || 3000 
-
+const express = require("express");
+const path = require("path");
+const hbs = require("hbs");
+const app = express();
+const geocode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
+const port = process.env.PORT || 3030;
 
 // Express config
-const publicDirPath =  path.join(__dirname,'../public')
-const viwePath = path.join(__dirname, '../templates/views')
-const partialsPath = path.join(__dirname, '../templates/partials')
-
-
+const publicDirPath = path.join(__dirname, "../public");
+const viwePath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
 
 //handlebars engine
-app.set('view engine', 'hbs')
-app.set('views',viwePath)
-hbs.registerPartials(partialsPath)
-
+app.set("view engine", "hbs");
+app.set("views", viwePath);
+hbs.registerPartials(partialsPath);
 
 //setup static to serve
-app.use(express.static(publicDirPath))
+app.use(express.static(publicDirPath));
 
+app.get("/help", (req, res) => {
+  res.render("help", {
+    helpText: "this is helpful text",
+    title: "Help",
+    name: "Khaled Gharib",
+  });
+});
 
-app.get('/help',(req,res)=>{
-    res.render('help',{
-        helpText:'this is helpful text',
-        title:"Help",
-        name:'Khaled Gharib'
-    })
-})
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About me",
+    name: "Khaled Gharib",
+  });
+});
 
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather",
+    name: "Khaled Gharib",
+  });
+});
 
-app.get('/about',(req,res)=>{
-    res.render('about',{
-        title:'About me',
-        name:"Khaled Gharib"
-    })
-})
+app.get("/weather", (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: "Can't leave search box empty",
+    });
+  }
 
-app.get('',(req,res)=>{
-    res.render('index',{
-        title:'Weathr',
-        name:'Khaled Gharib'
-    })
-})
+  geocode(req.query.address, (error, { lat, long, location } = {}) => {
+    if (error) return res.send({ error });
+    forecast(lat, long, (error, forcastData) => {
+      if (error) {
+        return res.send({ error });
+      }
+      res.send({
+        location,
+        forcastData,
+        address: req.query.address,
+      });
+    });
+  });
+});
 
+app.get("*", (req, res) => {
+  res.render("404", {
+    title: "Error 404. Page not found",
+    name: "Khaled Gharib",
+  });
+});
 
-app.get('/weather',(req,res)=>{
-    if(!req.query.address){
-        return res.send({
-            error:"Can't leave search box empty"
-        })
-    }
-
-    geocode(req.query.address,(error,{lat,long,location}={})=>{
-        if(error)
-        return res.send({ error})
-        forecast(lat , long,(error,forcastData)=>{
-            if(error){
-                return res.send({error})
-            }
-            res.send({
-            location,
-            forcastData,
-            address:req.query.address
-            })
-        })
-    })
-})
-
-
-
-app.get('*',(req,res)=>{
-    res.render('404',{
-        title:"Error 404. Page not found",
-        name:"Khaled Gharib"
-    })
-})
-
-
-
-app.listen(port,()=>{
-    console.log("server is up "+port)
-})
-
-
-
-
-
-
-
+app.listen(port, () => {
+  console.log("server is up " + port);
+});
--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,24 +1,27 @@
-const request = require('postman-request')
+const request = require("postman-request");
 
-const forecast = (lat,long,callback)=>{
-    // Set a new API
-    const url = 'http://api.weatherstack.com/current?access_key=979caf590fb10554e9657716adf021bb&query='+ lat +','+ long
-    request ({url,json:true},(error,{body}={})=>{
-        if(error){
-            callback("Please check your connection",undefined)
+const forecast = (lat, long, callback) => {
+  // Set a new API
+  const url =
+    "http://api.weatherstack.com/current?access_key=979caf590fb10554e9657716adf021bb&query=" +
+    lat +
+    "," +
+    long;
+  request({ url, json: true }, (error, { body } = {}) => {
+    if (error) {
+      callback("Please check your connection", undefined);
+    } else if (body.error) {
+      callback("Can't find location, Try another one", undefined);
+    } else {
+      const temp = body.current.temperature;
+      const feelslike = body.current.feelslike;
+      const weather_descriptions = body.current.weather_descriptions;
+      callback(
+        undefined,
+        `${weather_descriptions}. The temperatuer is ${temp}°. Feels like ${feelslike}°  `
+      );
+    }
+  });
+};
 
-        }else if(body.error){
-            callback("Can't find location, Try another one",undefined)
-        }else{
-            const temp = body.current.temperature
-            const feelslike =body.current.feelslike
-           const weather_descriptions= body.current.weather_descriptions
-        //    const localtime= body.location.localtime
-            callback(undefined,`${weather_descriptions}. The temperatuer is ${temp}°. Feels like ${feelslike}°  `)
-        }
-    })
-
-}
-
-
-module.exports=forecast
+module.exports = forecast;
